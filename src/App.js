12eab9525655f9aa17c6a122e98c23657efb6310
Павел <akumapl86@gmail.com>
2022-03-17 12:48:31 +0300

    init server

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import Counter from './Components/Counter';
 import './styles/app.css'
@@ -6,25 +6,10 @@ import './styles/app.css'
 
 
 function App() {
-    const [value, setValue] = useState('')
 
-    function inp(e) {
-        setValue(e.target.value)
-    }
 
     return (
         <div className="app_main">
-            <Counter />
-            <Counter />
-            <Counter />
-            <input
-                style="margin: 10px"
-                type="text"
-                placeholder="VALUE"
-                value={value}
-                onChange={inp}
-            ></input>
-            <h3>{value}</h3>
 
         </div>
     );
