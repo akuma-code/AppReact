2fe9cd5ee4a2b3a55ee6d3cdd4911148369e8e6c
akuma-code <akumapl86@gmail.com>
2022-03-18 03:09:38 +0300

    sync

---
+++ src/Components/navbar/NavItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavItem = () => {
+    return (
+        <button>
+
+        </button>
+    )
+}
+
+export default NavItem
---
+++ src/model/iTemplates.js
@@ -0,0 +1,8 @@
+//@ts-check
+
+
+export const okItem = (name = '', price = 0, options = {}) => { return { name, price, options } }
+export const okItem2 = (name = '', price = 0, options = {}) => { return { name, price, options } }
+
+
+export default { okItem, okItem2 }
