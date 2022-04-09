75333db035b52f754b836bc7d780b04c7dccba81
Павел <akumapl86@gmail.com>
2022-04-09 14:56:18 +0300

    sync

--- client/src/Components/UI/card/ShopItem.jsx
+++ client/src/Components/UI/card/ShopItem.jsx
@@ -1,10 +1,11 @@
+import { observer } from "mobx-react-lite";
 import React from 'react';
-import { Button, Card, ListGroup, ListGroupItem } from "react-bootstrap";
+import { Button, Card, ListGroup } from "react-bootstrap";
 import { useHistory } from "react-router-dom";
 import { removeShopPosition } from '../../../http/shopAPI';
 import { OKNO_ROUTE } from "../../../utils/consts";
 
-const ShopItem = ({ shopItem }) => {
+const ShopItem = observer(({ shopItem }) => {
     const history = useHistory()
     const { id, title, price, type } = shopItem;
 
@@ -12,52 +13,56 @@ const ShopItem = ({ shopItem }) => {
 
 
     return (
-        <Card style={{ width: '13rem' }} className="mt-2 mx-1">
+        <Card style={ { width: '13rem' } } className="mt-2 mx-1">
             <Card.Body
                 className='d-flex flex-column justify-content-between'
             >
-                <Card.Title as={"h4"}>{title}</Card.Title>
+                <Card.Title as="h4">{ title }</Card.Title>
                 <Card.Img
                     variant="top"
-                    src={type && `${process.env.REACT_APP_API_URL}/${type.img}`}
+                    src={ type && `${process.env.REACT_APP_API_URL}/${type.img}` }
                     alt="No Image"
 
                 />
-                <Card.Text className="text-center">
-                    <ListGroup>
-                        <ListGroupItem className='bg-warning font-weight-bold'>
-                            Цена: {price} руб.
-                        </ListGroupItem>
-                        {type.info && type.info.map((i, idx) =>
-                            <ListGroupItem key={idx}
-                                style={{
-                                    backgroundColor: (idx % 2 === 0) ? "lightgray" : "darkgray",
-                                    fontSize: 13
-                                }}
-                            >
-                                {i.title}  :  {i.desc}
-                            </ListGroupItem>)}
 
-                    </ListGroup>
-                </Card.Text>
+                <ListGroup >
+                    <ListGroup.Item className='bg-warning'>
+                        <div> Цена: { price } руб.</div>
+                    </ListGroup.Item>
+
+                    { type.info && type.info.map((i, idx) => (
+                        <ListGroup.Item
+                            key={ idx }
+                            style={ {
+                                backgroundColor: (idx % 2 === 0) ? "lightgray" : "darkgray",
+                                fontSize: 13
+                            } }
+
+                        >
+                            <p>{ i.title }  :  { i.desc }</p>
+                        </ListGroup.Item>)) }
+
+                </ListGroup>
+
             </Card.Body>
             <Card.Footer className="d-flex justify-content-between"
             >
                 <Button
                     variant="secondary"
                     size="sm"
-                    onClick={() => history.push(OKNO_ROUTE + '/' + id)}
+                    onClick={ () => history.push(OKNO_ROUTE + '/' + id) }
                 >
                     Открыть
                 </Button>
                 <Button variant="danger"
                     size="sm"
-                    onClick={() => removeShopPosition(id)}
+                    onClick={ () => removeShopPosition(id) }
                 >
-                    Удалить</Button>
+                    Удалить
+                </Button>
             </Card.Footer>
         </Card>
     );
-}
+})
 
 export default ShopItem;
--- client/src/Components/modals/CreateType.js
+++ client/src/Components/modals/CreateType.js
@@ -8,7 +8,7 @@ import { createType, fetchTypes } from "../../http/typesAPI"
 const CreateType = observer(({ show, onHide }) => {
     const { ogo } = useContext(Context)
 
-    const [typeName, setTypeName] = useState(ogo.selectedType.name);
+    const [typeName, setTypeName] = useState("");
     const [info, setInfo] = useState([]);
     const [file, setFile] = useState('')
 
@@ -44,8 +44,8 @@ const CreateType = observer(({ show, onHide }) => {
 
     return (
         <Modal
-            show={show}
-            onHide={onHide}
+            show={ show }
+            onHide={ onHide }
             centered
         >
             <Modal.Header closeButton>
@@ -58,50 +58,50 @@ const CreateType = observer(({ show, onHide }) => {
                     <Form.Control
                         className='mt-2 '
                         placeholder="название типа"
-                        value={typeName}
-                        onChange={(e) => setTypeName(e.target.value)}
+                        value={ typeName }
+                        onChange={ (e) => setTypeName(e.target.value) }
                     />
 
                     <Form.Control
                         className='mt-2 custom-file-input'
                         placeholder="изображение"
                         type='file'
-                        onChange={(e) => selectFile(e)}
+                        onChange={ (e) => selectFile(e) }
                     />
 
 
-                    {info.map(i =>
+                    { info.map(i =>
                         <Row className='mt-2'
-                            key={i.number}>
-                            <Col md={3}>
+                            key={ i.number }>
+                            <Col md={ 3 }>
                                 <Form.Control
                                     placeholder='название'
-                                    value={i.title}
-                                    onChange={(e) => changeInfo('title', e.target.value, i.number)}
+                                    value={ i.title }
+                                    onChange={ (e) => changeInfo('title', e.target.value, i.number) }
                                 />
                             </Col>
-                            <Col md={3}>
+                            <Col md={ 3 }>
                                 <Form.Control
                                     placeholder='описание'
-                                    value={i.desc}
-                                    onChange={(e) => changeInfo('desc', e.target.value, i.number)}
+                                    value={ i.desc }
+                                    onChange={ (e) => changeInfo('desc', e.target.value, i.number) }
                                 />
                             </Col>
-                            <Col md={3}>
+                            <Col md={ 3 }>
                                 <Button
-                                    variant={'outline-dark'}
+                                    variant={ 'outline-dark' }
                                     className='btn btn-danger'
-                                    onClick={() => removeInfo(i.number)}
+                                    onClick={ () => removeInfo(i.number) }
                                 >
                                     Удалить
                                 </Button>
                             </Col>
                         </Row>
-                    )}
+                    ) }
                     <Button
                         className="w-100 mt-2 btn-secondary"
-                        variant={'outline-dark'}
-                        onClick={addInfo}
+                        variant={ 'outline-dark' }
+                        onClick={ addInfo }
                     >
                         Добавить инфо
                     </Button>
@@ -110,10 +110,10 @@ const CreateType = observer(({ show, onHide }) => {
             <Modal.Footer className='d-flex justify-content-between'>
                 <Button
                     className='btn btn-success'
-                    variant={'outline-dark'}
-                    onClick={addOkType}
+                    variant={ 'outline-dark' }
+                    onClick={ addOkType }
                 >Добавить</Button>
-                <Button onClick={onHide}>Отмена</Button>
+                <Button onClick={ onHide }>Отмена</Button>
             </Modal.Footer>
         </Modal>
     );
