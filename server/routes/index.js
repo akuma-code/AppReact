bf5cfdee6caf4a32adcb244efbc257a19357fd8f
Павел <akumapl86@gmail.com>
2022-08-03 18:53:09 +0300

    add nets

--- server/routes/index.js
+++ server/routes/index.js
@@ -8,14 +8,14 @@ const typeRouter = require('./typeRouter')
 const shopRouter = require('./shopRouter')
 const pQueryRouter = require('./pQueryRouter')
 
-
+router.use('/nets', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+})
 router.use('/user', userRouter)
 router.use('/shop', shopRouter)
 router.use('/type', typeRouter)
 router.use('/sklad', skladRouter)
 router.use('/prod', pQueryRouter)
-router.use('/nets', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-})
+
 
 module.exports = router
