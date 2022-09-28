f206e52022e99b24e5d5510ef34ff4314d7b6d56
akuma-code <akumapl86@gmail.com>
2022-09-28 11:00:08 +0300

    sync

--- server/controllers/rootController.js
+++ server/controllers/rootController.js
@@ -1,6 +1,8 @@
 const ApiError = require("../Error/ApiError")
 
 const ROOTPASS = () => process.env.ROOT_PASS || "draksd1e"
+
+
 function check(user_pass) {
     const root_pass = ROOTPASS()
     const isEqual = user_pass === root_pass
@@ -11,9 +13,9 @@ class RootController {
         const { password } = req.body
         let comparePassword = check(password)
         const isAuth = comparePassword ? true : false
+        console.log(isAuth);
 
-
-        return res.json({ isAuth })
+        res.json(isAuth)
     }
 
 
